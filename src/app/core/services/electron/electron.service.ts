df2fffdee55364be02b68f3809dc4195807e97f8
mayiwen <i@mayiwen.com>
2023-10-03 13:17:12 +0800

    2023-10-03

--- src/app/core/services/electron/electron.service.ts
+++ src/app/core/services/electron/electron.service.ts
@@ -2,28 +2,43 @@ import { Injectable } from '@angular/core';
 
 // If you import a module but never use any of the imported values other than as TypeScript types,
 // the resulting javascript file will look as if you never imported the module at all.
-import { ipcRenderer, webFrame } from 'electron';
+import { ipcRenderer, webFrame, app, shell } from 'electron';
 import * as childProcess from 'child_process';
 import * as fs from 'fs';
-
+import * as zlib from 'zlib'
+import * as path from 'path'
+import * as compressing from 'compressing'
+import {autoUpdater} from 'electron-updater'
 @Injectable({
   providedIn: 'root'
 })
 export class ElectronService {
+
   ipcRenderer!: typeof ipcRenderer;
   webFrame!: typeof webFrame;
+  shell!: typeof shell;
+  app!: typeof app;
   childProcess!: typeof childProcess;
   fs!: typeof fs;
-
+  zlib!: typeof zlib;
+  path!: typeof path;
+  autoUpdater!: typeof autoUpdater;
+  /** 获取用户文件夹 */
+  pathUserDataString = ''
+  pathDesktopString = ''
+  compressing!: typeof compressing
   constructor() {
     // Conditional imports
     if (this.isElectron) {
-      this.ipcRenderer = (window as any).require('electron').ipcRenderer;
-      this.webFrame = (window as any).require('electron').webFrame;
-
-      this.fs = (window as any).require('fs');
-
-      this.childProcess = (window as any).require('child_process');
+      this.ipcRenderer = window.require('electron').ipcRenderer;
+      this.webFrame = window.require('electron').webFrame;
+      this.app = window.require('electron').app;
+      this.fs = window.require('fs');
+      this.zlib = window.require('zlib');
+      this.path = window.require('path');
+      this.compressing = window.require('compressing')
+      this.childProcess = window.require('child_process');
+      this.autoUpdater = window.require('electron-updater')
       this.childProcess.exec('node -v', (error, stdout, stderr) => {
         if (error) {
           console.error(`error: ${error.message}`);
@@ -35,6 +50,22 @@ export class ElectronService {
         }
         console.log(`stdout:\n${stdout}`);
       });
+      const returnValue = this.ipcRenderer.send('electronAppPathUserData', {});
+      this.ipcRenderer.on('electronAppPathUserDataReturn', (event: any, message: any) => {
+        console.log('这是返回的message')
+        console.log(message)
+        this.pathUserDataString = message;
+      })
+      this.ipcRenderer.on('electronAppPathDesktopReturn', (event: any, message: any) => {
+        console.log('pathDesktopString')
+        console.log(message)
+        this.pathDesktopString = message;
+      })
+      this.ipcRenderer.on('update', (event:any, message:any) => {
+        console.log('这是返回的数据。')
+        console.log(event)
+        console.log(message)
+      })
 
       // Notes :
       // * A NodeJS's dependency imported with 'window.require' MUST BE present in `dependencies` of both `app/package.json`
