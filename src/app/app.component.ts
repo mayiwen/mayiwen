49508224c2909b200619a09961f92fdb5423d913
mayiwen <i@mayiwen.com>
2023-11-25 10:54:21 +0800

    2023-11-25

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ElectronService } from './core/services';
 import { TranslateService } from '@ngx-translate/core';
 import { APP_CONFIG } from '../environments/environment';
@@ -7,7 +7,8 @@ import { APP_CONFIG } from '../environments/environment';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  listeners: any
   constructor(
     private electronService: ElectronService,
     private translate: TranslateService
@@ -23,5 +24,34 @@ export class AppComponent {
     } else {
       console.log('Run in browser');
     }
+  }
+  ngOnInit(): void {
+    this.listeners={
+      dark:(mediaQueryList: any )=>{
+        if(mediaQueryList.matches){
+          console.log('您切换到深色模式了！')
+          window.document.documentElement.setAttribute('data-myw-theme', 'black');
+        }
+      },
+      light:(mediaQueryList: any)=>{
+        if(mediaQueryList.matches){
+          console.log('您切换到浅色模式了！')
+          window.document.documentElement.setAttribute('data-myw-theme', 'white');
+
+        }
+      }
+    }
+    if (window.matchMedia('(prefers-color-scheme)').media === 'not all') {
+      console.log('Browser doesn\'t support dark mode');
+    } else {
+      if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+        window.document.documentElement.setAttribute('data-myw-theme', 'black');
+      } else {
+        window.document.documentElement.setAttribute('data-myw-theme', 'white');
+      }
+    }
+    window.matchMedia('(prefers-color-scheme: dark)').addListener(this.listeners.dark)
+    window.matchMedia('(prefers-color-scheme: light)').addListener(this.listeners.light)
+
   }
 }
